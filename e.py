4be552a6132263a4ae4a58638b189369d943aafd
MaharLeika18 <starlou18@gmail.com>
2025-08-12 21:07:16 +0800

    Changed the locator for the read_csv. Added an ipynb version of the py file. Also added new Family column to data frame.

--- e.py
+++ e.py
@@ -2,20 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load dataset
-data = pd.read_csv("C:\\Users\\Arrcann\\Documents\\RapidMiner\\Titanic-Dataset (1).csv")
+# Load dataset 
+url = "https://raw.githubusercontent.com/MaharLeika18/Data-Mining---Python/refs/heads/main/Titanic-Dataset.csv"
+data = pd.read_csv(url)
 
 # Clean the data
 data.isnull().sum()
-data.drop_duplicaes()
+data.fillna(0)
+data.drop_duplicates()
+
+# Create a new column for Family
+data['Family'] = data['SibSp'] + data['Parch'] + 1
 
 # Summary statistics
-data.describe()
+print(data.describe())
 
-# Plot Survival Patterns by Category
-category_survivors = data.groupby('Category')['Survivors'].sum()
-category_survivors.plot(kind='bar')
-plt.title('Survivors by Pattern Category')
-plt.xlabel('Category')
-plt.ylabel('Total Survivors')
-plt.show()
+# Print first 20 rows
+df_first = data[:20]
+print(df_first)
+
+
