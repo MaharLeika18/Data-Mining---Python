4e831189f3fd4e704cf23f45b88a301e7c32f211
MaharLeika18 <168389818+MaharLeika18@users.noreply.github.com>
2025-10-09 13:01:11 +0800

    Dataset file fix
    Fixed (kinda) the formatting for the dataset. Added some preprocessing functions. Minor push so that csv file is updated.

--- Midterms_Act1/MidtermsExam/TextPreprocessing.py
+++ Midterms_Act1/MidtermsExam/TextPreprocessing.py
@@ -7,6 +7,8 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 import pandas as pd
+from collections import Counter
+import spacy
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -14,9 +16,10 @@ nltk.download('punkt_tab')
 nltk.download('wordnet')
 stemmer = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
+nlp = spacy.load("en_core_web_sm")
 
 def importdata():
-    url = ""
+    url = "https://raw.githubusercontent.com/MaharLeika18/Data-Mining---Python/refs/heads/Loue/Midterms_Act1/MidtermsExam/Jobs.csv"
     text = pd.read_csv(url)
 
     # Displaying dataset information
@@ -45,4 +48,24 @@ def text_cleaning(text):
     word_tokens = word_tokenize(text)   # Lemmatization
     text = [lemmatizer.lemmatize(word) for word in word_tokens]
 
+    # Create new column for categorized job positions
+    text["categorized_title"] = text["title"]
     
+def populate_categ_title(title):
+    doc = nlp(title)
+    nouns = [token.text.lower() for token in doc if token.pos_ in ['NOUN', 'PROPN']]
+    if not nouns:
+        return 'other'
+    # return most frequent noun
+    return Counter(nouns).most_common(1)[0][0]
+
+
+if __name__ == "__main__":
+    df = importdata().text_cleaning()
+
+    df['categorized_title'] = df['title'].apply(populate_categ_title)
+
+    
+# Visualization ideas:
+# Pie/Bar graph of platforms used to announce job postings
+# Pie/Bar of professions
